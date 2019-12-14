4b23f2bf992304b205b85430e0e2c7f6d1a7fd2a
Shamil Frontend <sh.alisultanov@maximatelecom.ru>
2019-12-14 23:23:44 +0300

    реализация метода post для класса Http

--- src/framework/tools/http.js
+++ src/framework/tools/http.js
@@ -2,10 +2,27 @@ class Http {
   get(url) {
     return sendRequest('GET', url);
   }
+
+  post(url, data) {
+    return sendRequest('POST', url, data)
+  }
 }
 
-function sendRequest(method, url) {
-  return fetch(url, { method })
+function sendRequest(method, url, data = {}) {
+  const METHODS_FOR_CHANGING_DATA = ['POST', 'PUT', 'DELETE'];
+
+  const params = {
+    method,
+  };
+
+  if (METHODS_FOR_CHANGING_DATA.includes(method)) {
+    params.body = data
+  }
+
+  return fetch(
+    url,
+    params
+  )
     .then(response => response.json());
 }
 
